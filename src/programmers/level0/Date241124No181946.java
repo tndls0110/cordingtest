8538b85522dfb8ac898cf5f4b35b016a338d82e9
suin <tndls@email.net>
2024-11-24 07:58:26 +0900

    Date-241124 No-181946

---
+++ src/programmers/level0/Date241124No181946.java
@@ -0,0 +1,17 @@
+package programmers.level0;
+
+import java.util.Scanner;
+
+// 118.53-131.40ms
+public class Date241124No181946 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 입력받은 값 저장
+        String a = sc.next();
+        String b = sc.next();
+        
+        // 출력
+        System.out.println(a + b);
+    }
+}
